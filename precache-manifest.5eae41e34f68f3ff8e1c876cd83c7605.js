0593ed3207a2dcdbf5f760abae31e2537b89ed7b
kimhjona <kimhjona@gmail.com>
2024-07-22 13:38:25 -0700

    Updates

---
+++ precache-manifest.5eae41e34f68f3ff8e1c876cd83c7605.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "880373774f0a5dede05c4f0853c79452",
+    "url": "/almost-hacker-news/index.html"
+  },
+  {
+    "revision": "4924145e14acb2fa374d",
+    "url": "/almost-hacker-news/static/css/main.a7f7c08c.chunk.css"
+  },
+  {
+    "revision": "9129b87926b055d6ff56",
+    "url": "/almost-hacker-news/static/js/2.999772f0.chunk.js"
+  },
+  {
+    "revision": "4924145e14acb2fa374d",
+    "url": "/almost-hacker-news/static/js/main.590a8146.chunk.js"
+  },
+  {
+    "revision": "db2fc7c8aeec039d70cb",
+    "url": "/almost-hacker-news/static/js/runtime~main.13ae506d.js"
+  }
+]);
